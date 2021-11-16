0af21adc139eca9455e8a1605df1dacc5419e73b
Jeffrey Liu <jliu5021@usc.edu>
2021-11-15 21:46:57 -0800

    First_Attempt_JL

--- youtube.py
+++ youtube.py
@@ -21,14 +21,18 @@ for l in videos:
 
     for i in range(1,3):
         try:
-            driver.get(toget)
-            driver.switch_to_window(driver.window_handles[0])
+             driver.get(toget)
+            # driver.switch_to_window(driver.window_handles[0])
+
             driver.refresh()
             html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
-            commas = str(html).split(",")
+            commas = str(html).split(">")
             for line in commas:            
-                if('dVideoId' in str(line)):
-                    print(l,"||",line)
+                if('meta name=\"keywords\"' in str(line)):
+                    line_sub = line[22:]
+                    # print(l,"||", line)
+                    print(l,"||", line_sub)
+
             #print(html)
             time.sleep(5)
             driver.refresh()
