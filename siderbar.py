08279ffc41d160278319ad49591e050233ec223a
Jeffrey Liu <jliu5021@usc.edu>
2022-03-06 01:08:29 -0800

    New_CHange_SiderBar_JL

--- siderbar.py
+++ siderbar.py
@@ -5,9 +5,12 @@ from selenium.common.exceptions import StaleElementReferenceException
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 
+import json
 videos = ['hkaXd07-rTU']
 
 prev = ""
+
+info = []
 
 for l in videos:
     toget = "https://www.youtube.com/watch?v=" + l
@@ -15,16 +18,54 @@ for l in videos:
 
 
     chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     chrome_options.add_argument("--public")
     driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
 
     for i in range(1,2):
-        try:
-            driver.get(toget)
-            html = driver.page_source
-            print(str(html))
+        
+        driver.get(toget)
+        #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        html = driver.execute_script("return document.getElementsByTagName('html')[0 ].innerHTML")
+        
+        all_info = str(html)
+        fname = "result.txt"
 
-            time.sleep(5000)
-        except:
-            continue
+        if "promotedSparklesWebRenderer" in all_info:
+            place = all_info.find("promotedSparklesWebRenderer")
+            info = all_info[place:place+2000]
+            if "title" in info:
+                title_place = info.find("title")
+                end_place = info[title_place:].find("}")
+                line_sub_title = info[title_place+22:title_place+end_place-1]
+                
+            if "description" in info:
+                title_place = info.find("description")
+                end_place = info[title_place:].find("}")
+                line_sub_description = info[title_place+28:title_place+end_place-1]
+            
+
+            if "websiteText" in info:
+                title_place = info.find("websiteText")
+                end_place = info[title_place:].find("}")
+                line_sub_website = info[title_place+28:title_place+end_place-1]
+                
+            
+
+        with open(fname, "w", encoding="utf-8") as f:
+            start = 100
+            f.write(f"Title: {line_sub_title}\n")
+            f.write(f"Description: {line_sub_description}\n")
+            f.write(f"Website: {line_sub_website}\n")
+            f.write("-----------Below is the whole text-------------\n\n")
+
+            # while start <= len(info):
+            #     f.write(info[start-100:start])
+            #     f.write("\n")
+            #     start += 100
+            f.write(all_info)
+
+        time.sleep(5)            
+        # except:
+        #     continue
     driver.close()
