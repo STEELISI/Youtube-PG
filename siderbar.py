74d04e6296e9f8dd254b5dad3c883971eb855a31
Jeffrey Liu <jliu5021@usc.edu>
2022-03-09 22:15:35 -0800

    With Changes to output the advertisements' info in correct format

--- siderbar.py
+++ siderbar.py
@@ -6,8 +6,14 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 
 import json
-videos = ['hkaXd07-rTU']
 
+#extract videos from a file
+videos = set()
+f = open("vid.txt")
+for p in f:
+    videos.add(p.strip())
+
+# videos = ['hkaXd07-rTU','ldjtA3hj5cc','XSvBYVjgtGs']
 prev = ""
 
 info = []
@@ -23,49 +29,50 @@ for l in videos:
     driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
 
     for i in range(1,2):
-        
-        driver.get(toget)
-        #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        html = driver.execute_script("return document.getElementsByTagName('html')[0 ].innerHTML")
-        
-        all_info = str(html)
-        fname = "result.txt"
+        try:
+            driver.get(toget)
+            #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            html = driver.execute_script("return document.getElementsByTagName('html')[0 ].innerHTML")
 
-        if "promotedSparklesWebRenderer" in all_info:
-            place = all_info.find("promotedSparklesWebRenderer")
-            info = all_info[place:place+2000]
-            if "title" in info:
-                title_place = info.find("title")
-                end_place = info[title_place:].find("}")
-                line_sub_title = info[title_place+22:title_place+end_place-1]
-                
-            if "description" in info:
-                title_place = info.find("description")
-                end_place = info[title_place:].find("}")
-                line_sub_description = info[title_place+28:title_place+end_place-1]
-            
+            all_info = str(html)
+            fname = "result.txt"
 
-            if "websiteText" in info:
-                title_place = info.find("websiteText")
-                end_place = info[title_place:].find("}")
-                line_sub_website = info[title_place+28:title_place+end_place-1]
-                
-            
+            if "promotedSparklesWebRenderer" in all_info:
+                place = all_info.find("promotedSparklesWebRenderer")
+                info = all_info[place:place+2000]
+                if "title" in info:
+                    title_place = info.find("title")
+                    end_place = info[title_place:].find("}")
+                    line_sub_title = info[title_place+22:title_place+end_place-1]
 
-        with open(fname, "w", encoding="utf-8") as f:
-            start = 100
-            f.write(f"Title: {line_sub_title}\n")
-            f.write(f"Description: {line_sub_description}\n")
-            f.write(f"Website: {line_sub_website}\n")
-            f.write("-----------Below is the whole text-------------\n\n")
+                if "description" in info:
+                    title_place = info.find("description")
+                    end_place = info[title_place:].find("}")
+                    line_sub_description = info[title_place+28:title_place+end_place-1]
 
-            # while start <= len(info):
-            #     f.write(info[start-100:start])
-            #     f.write("\n")
-            #     start += 100
-            f.write(all_info)
 
-        time.sleep(5)            
-        # except:
-        #     continue
-    driver.close()
+                if "websiteText" in info:
+                    title_place = info.find("websiteText")
+                    end_place = info[title_place:].find("}")
+                    line_sub_website = info[title_place+28:title_place+end_place-1]
+
+
+
+            with open(fname, "a", encoding="utf-8") as f:
+                start = 100
+                f.write(f"{l} |")
+                f.write(f"{line_sub_title} | ")
+                f.write(f"{line_sub_description} | ")
+                f.write(f"{line_sub_website} \n")
+                # f.write("-----------Below is the whole text-------------\n\n")
+
+                # # while start <= len(info):
+                # #     f.write(info[start-100:start])
+                # #     f.write("\n")
+                # #     start += 100
+                # f.write(all_info)
+
+            time.sleep(5)            
+        except:
+            continue
+    driver.close() 
